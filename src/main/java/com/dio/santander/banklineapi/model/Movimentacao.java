17f215504709af9659320139a945191aa5762873
rrrs21 <rrrs21@gmail.com>
2022-05-03 23:09:21 -0300

    Alteração da tabela movimentação para inclusão do ID da conta

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -23,6 +23,10 @@ public class Movimentacao {
 	private Double valor;
 	@Enumerated (EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column (name = "id_conta")
+	private Integer idConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -53,6 +57,11 @@ public class Movimentacao {
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
-	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 
 }
